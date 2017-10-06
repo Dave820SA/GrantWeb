ab7417656fb82e326545fc8a91c3ca81cfc3700c
Dave820SA <dwdavidson@live.com>
2017-10-06 14:55:02 -0500

    End of  day and week 10/6

---
+++ PREP/App_Start/BundleConfig.cs
@@ -0,0 +1,73 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PREP
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/googlejquery", "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                        "~/Scripts/moment.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datepick").Include(
+                        "~/Scripts/bootstrap-datetimepicker.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerys").Include(
+                        "~/Scripts/jquery-1.9.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fullcalendarjs").Include(
+                     "~/Scripts/jquery-ui-1.12.1.min.js",
+                     "~/Scripts/fullcalendar.min.js"));
+
+
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
+
+            //Calendar css file
+            bundles.Add(new StyleBundle("~/Content/fullcalendarcss").Include(
+                     "~/Content/themes/jquery.ui.all.css",
+                     "~/Content/fullcalendar.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/solar").Include("~/Content/Solar.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/Slate.css"));
+
+            bundles.Add(new StyleBundle("~/Content/customcss").Include("~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
